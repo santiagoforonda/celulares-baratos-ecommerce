4fe1815966de6d8cb46463c80e26782fd0f25873
santiago <sergioforonda1894@gmail.com>
2026-04-20 15:56:03 -0500

    consumiendo y mostrando la informacion del backend

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,8 @@
 
 //Funcion para formatear moneda
 
+import type { Color, Product, Variant } from "../interfaces/product.interface";
+
 export const formatPrice =(price:number)=>{
     return new Intl.NumberFormat("en-US",{
         style:"currency",
@@ -9,3 +11,45 @@ export const formatPrice =(price:number)=>{
         maximumFractionDigits:2,
     }).format(price);
 }
+
+export const prepareProducts = (products: Product[]) => {
+	return products.map(product => {
+		// Agrupar las variantes por color
+		const colors = product.variants.reduce(
+			(acc: Color[], variant: Variant) => {
+				const existingColor = acc.find(
+					item => item.color === variant.color
+				);
+
+				if (existingColor) {
+					// Si ya existe el color, comparamos los precios
+					existingColor.price = Math.min(
+						existingColor.price,
+						variant.price
+					);
+				} // Mantenemos el precio mínimo
+				else {
+					acc.push({
+						color: variant.color,
+						price: variant.price,
+						name: variant.color_name,
+					});
+				}
+
+				return acc;
+			},
+			[]
+		);
+
+		// Obtener el precio más bajo de las variantes agrupadas
+		const price = Math.min(...colors.map(item => item.price));
+
+		// Devolver el producto formateado
+		return {
+			...product,
+			price,
+			colors: colors.map(({ name, color }) => ({ name, color })),
+			variants: product.variants,
+		};
+	});
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,27 +2,30 @@ import { Brands } from "../components/home/Brands"
 import { FeatureGrid } from "../components/home/FeatureGrid"
 import { ProductGrid } from "../components/home/ProductGrid"
 import { ProductGridSkeleton } from "../components/skeletons/ProductGridSkeleton"
+import { prepareProducts } from "../helpers"
 import { useHomeProducts } from "../hooks/products/useHomeProducts"
 
 
 export const HomePage = () => {
   
-  const {recentProducts,popularProducts,isLoading,isError} = useHomeProducts();
+  const {recentProducts,popularProducts,isLoading} = useHomeProducts();
 
-  
+  const preparedRecentsProducts = prepareProducts(recentProducts);
+  const preparedPopularProducts = prepareProducts(popularProducts);
+
   return (
     <section>
       <FeatureGrid></FeatureGrid>
     {
       isLoading? (<ProductGridSkeleton numberOfProducts={4}></ProductGridSkeleton>):(
-        <ProductGrid title="Nuevos productos" products={}></ProductGrid>
+        <ProductGrid title="Nuevos productos" products={preparedRecentsProducts}></ProductGrid>
       )
     }
 
     {
 
       isLoading ? (<ProductGridSkeleton numberOfProducts={4}></ProductGridSkeleton>):(
-        <ProductGrid title="Productos destacados" products={}></ProductGrid>
+        <ProductGrid title="Productos destacados" products={preparedPopularProducts}></ProductGrid>
       )
     }
       
